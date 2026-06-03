451cf0c30c60e3d6d7e870617c175b421f98be7f
Arnob Kumar Saha <arnob@appscode.com>
2026-06-03 17:12:16 +0600

    Remove alertmanager.webhook.url (#276)
    Signed-off-by: Arnob kumar saha <arnob@appscode.com>

--- apis/installer/v1alpha1/types.go
+++ apis/installer/v1alpha1/types.go
@@ -97,7 +97,6 @@ type AlertmanagerEmailSpec struct {
 
 type AlertmanagerWebhookSpec struct {
 	Enabled      bool                         `json:"enabled"`
-	URL          string                       `json:"url"`
 	SendResolved bool                         `json:"sendResolved"`
 	Relay        AlertmanagerWebhookRelaySpec `json:"relay"`
 }
@@ -106,17 +105,21 @@ type AlertmanagerWebhookRelaySpec struct {
 	ReplicaCount        int32                                 `json:"replicaCount"`
 	Port                int32                                 `json:"port"`
 	Image               ContianerRef                          `json:"image"`
-	Providers           AlertmanagerWebhookRelayProvidersSpec `json:"providers"`
-	RequestTimeout      string                                `json:"requestTimeout"`
-	DedupeCacheSize     int32                                 `json:"dedupeCacheSize"`
-	DedupeWindowSeconds int32                                 `json:"dedupeWindowSeconds"`
-	MaxRequestBodyBytes int64                                 `json:"maxRequestBodyBytes"`
+	Providers           AlertmanagerWebhookRelayProvidersSpec `json:"providers,omitempty"`
+	RequestTimeout      string                                `json:"requestTimeout,omitempty"`
+	DedupeCacheSize     int32                                 `json:"dedupeCacheSize,omitempty"`
+	DedupeWindowSeconds int32                                 `json:"dedupeWindowSeconds,omitempty"`
+	MaxRequestBodyBytes int64                                 `json:"maxRequestBodyBytes,omitempty"`
 }
 
 type AlertmanagerWebhookRelayProvidersSpec struct {
+	// +optional
 	GoogleChat AlertmanagerWebhookRelayProviderSpec `json:"googleChat"`
-	Slack      AlertmanagerWebhookRelayProviderSpec `json:"slack"`
-	MsTeams    AlertmanagerWebhookRelayProviderSpec `json:"msTeams"`
+	// +optional
+	Slack AlertmanagerWebhookRelayProviderSpec `json:"slack"`
+	// +optional
+	MsTeams AlertmanagerWebhookRelayProviderSpec `json:"msTeams"`
+	// +optional
 	Mattermost AlertmanagerWebhookRelayProviderSpec `json:"mattermost"`
 }
 
